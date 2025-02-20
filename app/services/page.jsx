b4949a6d22f09cc0213c75365e4f5e2f7a6fbaa9
Vinicius Penedo <vinicispenedo2003@gmail.com>
2025-02-19 21:57:54 -0300

    correção de bugs em service

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -33,7 +33,7 @@ const services = [
 
 const Services = () => {
     return (
-        <section className="min-h-[80vh] flex flex-col justoify-center pv-12 xl:py-0">
+        <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
     <div className="container mx-auto">
         <motion.div 
          initial={{ opacity: 0}}
@@ -47,13 +47,16 @@ const Services = () => {
                     <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
                         {/*top*/}
                         <div className="w-full flex justify-between items-center">
-                            <div className="text-5x1 font-extrabold text-outline text-transparent group-hover:text transition-all duration-500 ">{service.num}</div>
-                            <Link href={service.num} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-flex justify-center items-center hover:-rotate-45">     
-                            <BsArrowDownRight className="text-3x1 text-primary" />
-                            </Link>
+                        <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text transition-all duration-500">
+                        {service.num}</div>
+                        <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                            <BsArrowDownRight className="text-3xl text-primary" />
+                        </Link>
+
                         </div>
                         {/*itle*/}
-                        <h2 className="text-[42PX] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
+                        <h2 className="h2 text-white group-hover:text-accent transition-all duration-500">
+                        {service.title}</h2>
                         {/*description*/}
                         <p className="text-white/60">{service.description}</p>
                         {/*border*/}
