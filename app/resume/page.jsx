79ba9afcc06bcde3ffd11fe00c954a4be8eb1939
Vinicius Penedo <vinicispenedo2003@gmail.com>
2025-02-19 22:22:01 -0300

    começando a pagina resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,54 @@
+"use client";
+
+import { 
+    FaHtml5, 
+    FaCss3, 
+    FaJs, 
+    FaReact, 
+    FaFigma, 
+    FaNodeJs 
+} from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+const about ={
+    title: "About me",
+    description: "I am a front-end developer with a passion for creating beautiful, responsive, and user-friendly websites. I have experience with HTML, CSS, JavaScript, React, and Next.js. I am always looking for new challenges and opportunities to learn and grow as a developer.",
+    Info: [
+        {
+          fieldname: "Name",
+          fieldvalue: "Vinicius",
+        },
+        {
+          fieldname: "Phone",
+          fieldvalue: "(11) 95895-3041",
+        },
+        {
+            fieldname: "Experience",
+            fieldvalue: "1 year",
+        },
+        {
+            fieldname: "Instagram",
+            fieldvalue: "Vini.Penedo",
+        },
+        {
+            fieldname: "Nationality",
+            fieldvalue: "Brazilian",
+        },
+        {
+            fieldname: "email",
+            fieldvalue: "Viniciuspenedo2003@gmail.com",
+        },
+        {
+            fieldname: "Freelance",
+            fieldvalue: "available",
+        },
+        {
+            fieldname: "Language",
+            fieldvalue: "Portuguese, English",
+        },
+    ]
+}
+
 const Resume = () => {
     return <div>resume page</div>
 };
